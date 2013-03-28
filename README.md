36caaf35c759ad0204d7973aff13802575754489
Andrew Fecheyr <andrew@bedesign.be>
2013-03-28 09:45:05 +0100

    Simple mail catcher cookbook with upstart script

---
+++ README.md
@@ -0,0 +1,15 @@
+# mailcatcher cookbook
+
+Chef cookbook that installs mailcatcher and installs a service to run it on startup
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Andrew Fecheyr (<andrew@bedesign.be>)
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             "mailcatcher"
+maintainer       "Andrew Fecheyr"
+maintainer_email "andrew@bedesign.be"
+license          "MIT"
+description      "Installs Mailcatcher"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+depends          "build-essential"
+supports         "ubuntu"
