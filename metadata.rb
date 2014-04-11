9dcd8de122fe3e1a9a670beb58a7112329dd5b6e
Andrew Fecheyr <andrew@bedesign.be>
2014-04-11 16:33:12 +0200

    v0.3.0: mailcatcher bin attribute

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "andrew@bedesign.be"
 license          "MIT"
 description      "Installs Mailcatcher"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.3.0"
 depends          "build-essential"
 supports         "ubuntu"
 
