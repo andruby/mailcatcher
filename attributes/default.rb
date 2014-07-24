b37e45f2870c4bf4b2aad4525aa74228609b91bd
MK <kohenkatz@gmail.com>
2014-07-24 13:23:08 -0400

    more options

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,7 @@
 default['mailcatcher']['http_ip'] = '0.0.0.0'
+default['mailcatcher']['http-port'] = 1080
+
+default['mailcatcher']['smtp-ip'] = '127.0.0.1'
+default['mailcatcher']['smtp-port'] = 1025
+
 default['mailcatcher']['bin'] = 'mailcatcher'
