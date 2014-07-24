e76d1c410c1b270678d1b827be96edbe0040eb08
MK <kohenkatz@gmail.com>
2014-07-24 13:59:08 -0400

    fix mixed dashes and underscores

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['mailcatcher']['http_ip'] = '0.0.0.0'
-default['mailcatcher']['http-port'] = 1080
+default['mailcatcher']['http_port'] = 1080
 
-default['mailcatcher']['smtp-ip'] = '127.0.0.1'
-default['mailcatcher']['smtp-port'] = 1025
+default['mailcatcher']['smtp_ip'] = '127.0.0.1'
+default['mailcatcher']['smtp_port'] = 1025
 
 default['mailcatcher']['bin'] = 'mailcatcher'
